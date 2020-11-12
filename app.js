851ea5b2da6a94310321da20599d3dd9d296c3cb
Jin Park <jpark@recarboninc.com>
2020-11-12 14:58:12 -0800

    vue implemented

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+	data() {
+		return {
+			goals: [],
+			enteredValue: ''
+		};
+	}, //has to be called data
+	methods: {
+		addGoal() {
+			this.goals.push(this.enteredValue); // Vue establishes connection to recognize 'this'
+			this.enteredValue = '';
+		}
+	}
+}).mount('#app'); // Let Vue know which part of the app it should control. Div id
 
-function addGoal() {
-	const enteredValue = inputEl.value;
-	const listItemEl = document.createElement('li');
-	listItemEl.textContent = enteredValue;
-	listEl.appendChild(listItemEl);
-	inputEl.value = '';
-}
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-buttonEl.addEventListener('click',addGoal);
+// function addGoal() {
+// 	const enteredValue = inputEl.value;
+// 	const listItemEl = document.createElement('li');
+// 	listItemEl.textContent = enteredValue;
+// 	listEl.appendChild(listItemEl);
+// 	inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click',addGoal);
